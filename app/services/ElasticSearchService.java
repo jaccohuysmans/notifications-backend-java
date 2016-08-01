b56f33c12dc8f493a6607ee4e598816afc75e1dc
Jacco Huysmans <j.huysmans@sping.nl>
2016-08-01 16:18:07 +0200

    Some filename refactoring

--- app/services/ElasticSearchService.java
+++ app/services/ElasticSearchService.java
@@ -2,16 +2,47 @@ package services;
 
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.inject.ImplementedBy;
+import play.Play;
 import play.libs.F.Promise;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
+import play.libs.ws.WSResponse;
 import play.mvc.Result;
 
+import javax.inject.Inject;
 
-@ImplementedBy(RealElasticSearchService.class)
-public interface SearchService {
+import static play.mvc.Results.ok;
 
-  public Promise<Result> save(JsonNode json);
 
-  public Promise<Result> list();
+public class ElasticSearchService implements SearchService {
+
+  private String esUrl = Play.application().configuration().getString("elasticSearch.url");
+  private String esIndex = Play.application().configuration().getString("elasticSearch.index");
+  private String esType = Play.application().configuration().getString("elasticSearch.type");
+
+  private WSClient ws;
+
+  @Inject
+  public ElasticSearchService(WSClient ws) {
+    this.ws = ws;
+   }
+
+  public Promise<Result> save(JsonNode json){
+    Promise<WSResponse> jsonPromise = getWsRequest().post(json);
+    return jsonPromise.map(jsonResult -> { return ok(jsonResult.asJson());});
+  };
+
+  public Promise<Result> list() {
+    Promise<WSResponse> jsonPromise = getWsRequest("_search").get();
+    return jsonPromise.map(jsonResult -> { return ok(jsonResult.asJson());});
+  }
+
+  private WSRequest getWsRequest(){
+    return this.getWsRequest("");
+  }
+
+  private WSRequest getWsRequest(String command) {
+    return this.ws.url(this.esUrl+ "/" + this.esIndex +"/" + this.esType +"/" + command).setHeader("Content-Type", "application/json");
+  }
 
 }
